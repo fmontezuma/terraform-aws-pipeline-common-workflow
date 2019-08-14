16659d4699d4dd319a3b63098526740ef2d632d7
Fabio Montezuma <fabio.montezuma@soaexpert.com.br>
2019-08-14 19:10:38 -0300

    Cross account access

--- ecr.tf
+++ ecr.tf
@@ -1,3 +1,30 @@
 resource "aws_ecr_repository" "ecr" {
   name = "${var.project_name}-${var.microservice_name}"
 }
+
+resource "aws_ecr_repository_policy" "cross_account" {
+  repository = "${aws_ecr_repository.ecr.name}"
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "cross-account-access",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:GetRepositoryPolicy",
+                "ecr:DescribeRepositories",
+                "ecr:ListImages",
+                "ecr:DescribeImages",
+                "ecr:BatchGetImage"
+            ]
+        }
+    ]
+}
+EOF
+}
